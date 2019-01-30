f21f8a42a5035ca0472c1fb5b9a4964a724a6280
RGB Studios <justingolden21@gmail.com>
2019-01-30 01:54:38 -0800

    added slick, fixed scaling

--- scripts.js
+++ scripts.js
@@ -1,17 +1,34 @@
+let isSlick = false;
 
-//$('#title-header').css('color','#f00');
+$(function() {
 
-window.onload = function() {
+	// $('#title-header').css('color','#f00');
 
 	$('#color-detect-input').select();
 
-	$('#copy-link-btn').hover(function() {
-		$(this).html('<i class="fas fa-link"></i> Copy Link to Your Color');
-	});
-	$('#copy-link-btn').mouseleave(function() {
-		$(this).html('<i class="fas fa-link"></i> Copy Link');
-	});
+	checkSlick();
+});
 
+$(window).resize(checkSlick);
+
+function checkSlick() {
+	if(window.innerWidth < 992) {
+		if(!isSlick) {
+			$('#color-sections').slick({
+				dots: true,
+				arrows: true,
+				mobileFirst: true
+			});
+			$('input[type=range').css('display','none');
+		}
+		isSlick = true;
+	} else {
+		if(isSlick) {
+			$('#color-sections').slick('unslick');
+			$('input[type=range').css('display','');
+		}
+		isSlick = false;
+	}	
 }
 
 window.onkeyup = function(e) {
