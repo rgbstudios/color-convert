fac8f6c2f88055611984b114bb73348b9163b5b7
Justin Golden <justingolden21@gmail.com>
2020-08-19 19:38:55 -0700

    organizing code and files

--- js/util.js
+++ js/util.js
@@ -4,23 +4,24 @@ function randInt(min, max) {
 
 let toastIdx = 0;
 function makeToast(title, body) {
-	$('#toast-wrapper').append(
-		'<div id="toast-' + (++toastIdx) + '" class="toast m-auto" data-autohide="false">'
-	+	'<div class="toast-header">'
-	+		'<h5 class="mr-auto">' + title + '</h5>'
-	+		'<button type="button" class="close btn px-1" data-dismiss="toast" tabindex="-1">'
-	+			'<i class="fas fa-times fa-xs"></i>'
-	+		'</button>'
-	+	'</div>'
-	+	'<div class="toast-body">'
-	+		body
-	+	'</div>'
-	+	'</div>'
-	);
+	toastIdx++;
+	let toastID = 'toast-' + toastIdx;
+	$('#toast-wrapper').append(`
+		<div id="${toastID}" class="toast m-auto" data-autohide="false">
+		<div class="toast-header">
+			<h5 class="mr-auto">${title}</h5>
+			<button type="button" class="close btn px-1" data-dismiss="toast" tabindex="-1">
+				<i class="fas fa-times fa-xs"></i>
+			</button>
+		</div>
+		<div class="toast-body">
+			${body}
+		</div>
+		</div>
+	`);
 
-	let toastID = '#toast-' + toastIdx;
-	$(toastID).toast('show');
-	setTimeout( ()=> $(toastID).toast('hide'), 2500);
+	$('#' + toastID).toast('show');
+	setTimeout( ()=> $('#' + toastID).toast('hide'), 2500);
 }
 
 function copyText(str) {
@@ -47,8 +48,7 @@ function capitalize(word) {
 function toggleFullscreen() {
 	if(!document.fullscreenElement) {
 		document.documentElement.requestFullscreen();
-	}
-	else if(document.exitFullscreen) {
+	} else if(document.exitFullscreen) {
 		document.exitFullscreen(); 
 	}
 }
